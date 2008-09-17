e549dc42bde375b0a6933d5ea8d79b1f8ee22f6b
redsolo <redsolo@dev.java.net>
2008-09-17 21:55:59 +0000

    Added unit test that verifies that the profile id is stored properly

--- src/test/java/hudson/plugins/google/analytics/FooterWebTest.java
+++ src/test/java/hudson/plugins/google/analytics/FooterWebTest.java
@@ -4,6 +4,8 @@ import hudson.model.PageDecorator;
 import org.jvnet.hudson.test.HudsonTestCase;
 
 import com.gargoylesoftware.htmlunit.WebAssert;
+import com.gargoylesoftware.htmlunit.html.HtmlButton;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 public class FooterWebTest extends HudsonTestCase {
@@ -43,5 +45,26 @@ public class FooterWebTest extends HudsonTestCase {
         WebAssert.assertInputContainsValue(page, "_.profileId", "");
         assertFalse("The page text contained the profile", 
                 page.asXml().contains("_gat._getTracker("));
+    }
+
+    /**
+     * Asserts that the profile id for decorator is updated when submitted
+     */
+    public void testSubmittingConfigurationUpdatesProfileId() throws Exception {
+        pageDecorator = new GoogleAnalyticsChild("111");
+        PageDecorator.ALL.add(pageDecorator);
+        WebClient webClient = new WebClient();
+        HtmlForm form = webClient.goTo("configure").getFormByName("config");
+        form.getInputByName("_.profileId").setValueAttribute("NewProfile");
+        form.submit((HtmlButton)last(form.getHtmlElementsByTagName("button")));
+        assertEquals("The new profile id wasnt correct", "NewProfile", pageDecorator.getProfileId());
+    }
+    
+    // Class that has a different footer.jelly file than the real page decorator
+    // to ignore the javascript errors.
+    private static class GoogleAnalyticsChild extends GoogleAnalyticsPageDecorator {
+        public GoogleAnalyticsChild(String string) {
+            super(string);
+        }
     }
 }
